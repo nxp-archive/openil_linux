f73776425e28f41cf23d9425648ddcb2510fea5e
Ramneek Mehresh <ramneek.mehresh@freescale.com>
2013-04-11 17:07:31 +0530

    fsl/otg: Remove host drv upon otg bring-up
    Change have_hcd variable to remove/suspend host driver on
    completion of otg initilization for otg auto detect
    
    Signed-off-by: Ramneek Mehresh <ramneek.mehresh@freescale.com>
    Change-Id: Ibc1c492629eb3afffb43dcb0a574783ad1b209f0
    Reviewed-on: http://git.am.freescale.net:8181/1371
    Reviewed-by: Li Yang-R58472 <LeoLi@freescale.com>
    Reviewed-by: Fleming Andrew-AFLEMING <AFLEMING@freescale.com>
    Tested-by: Fleming Andrew-AFLEMING <AFLEMING@freescale.com>

--- drivers/usb/host/ehci-fsl.c
+++ drivers/usb/host/ehci-fsl.c
@@ -177,6 +177,7 @@ static int usb_hcd_fsl_probe(const struct hc_driver *driver,
 #if defined(CONFIG_FSL_USB2_OTG) || defined(CONFIG_FSL_USB2_OTG_MODULE)
 	if (pdata->operating_mode == FSL_USB2_DR_OTG) {
 		struct ehci_hcd *ehci = hcd_to_ehci(hcd);
+		struct ehci_fsl *ehci_fsl = hcd_to_ehci_fsl(hcd);
 
 		hcd->usb_phy = usb_get_phy(USB_PHY_TYPE_USB2);
 
@@ -197,6 +198,11 @@ static int usb_hcd_fsl_probe(const struct hc_driver *driver,
 			retval = -ENODEV;
 			goto err2;
 		}
+
+		ehci_fsl->have_hcd = 1;
+	} else {
+		dev_err(&pdev->dev, "wrong operating mode\n");
+		return -ENODEV;
 	}
 #endif
 	return retval;
