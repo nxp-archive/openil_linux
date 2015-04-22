d29646e30872af9b53ad8384914e9eafbfc7be01
Haiying Wang <Haiying.Wang@freescale.com>
2015-04-22 13:09:47 -0400

    arm64: add support to remap kernel cacheable memory to userspace
    Signed-off-by: Haiying Wang <Haiying.Wang@freescale.com>
    Change-Id: I50ee4798a2929932fa9ff7c9cdb42cd1a215f77a
    Reviewed-on: http://git.am.freescale.net:8181/35488
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Roy Pledge <roy.pledge@freescale.com>
    Reviewed-by: Stuart Yoder <stuart.yoder@freescale.com>

--- arch/arm64/include/asm/pgtable.h
+++ arch/arm64/include/asm/pgtable.h
@@ -324,6 +324,9 @@ static inline int has_transparent_hugepage(void)
 	__pgprot_modify(prot, PTE_ATTRINDX_MASK, PTE_ATTRINDX(MT_DEVICE_nGnRnE) | PTE_PXN | PTE_UXN)
 #define pgprot_writecombine(prot) \
 	__pgprot_modify(prot, PTE_ATTRINDX_MASK, PTE_ATTRINDX(MT_NORMAL_NC) | PTE_PXN | PTE_UXN)
+#define pgprot_cached(prot) \
+	__pgprot_modify(prot, PTE_ATTRINDX_MASK, PTE_ATTRINDX(MT_NORMAL) | \
+			PTE_PXN | PTE_UXN)
 #define pgprot_device(prot) \
 	__pgprot_modify(prot, PTE_ATTRINDX_MASK, PTE_ATTRINDX(MT_DEVICE_nGnRE) | PTE_PXN | PTE_UXN)
 #define __HAVE_PHYS_MEM_ACCESS_PROT
