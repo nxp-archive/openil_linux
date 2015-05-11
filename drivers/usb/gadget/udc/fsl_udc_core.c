e1b05fa2b2d3bf8b2e39ca2656b0fb09a1336183
Nikhil Badola <nikhil.badola@freescale.com>
2015-05-11 17:21:27 +0530

    drivers:usb:fsl: Correct endpoint name checking
    Endpoint name being statically defined cannot be NULL
    hence checking name's first two character with "ep" for
    endpoint validity
    
    Signed-off-by: Nikhil Badola <nikhil.badola@freescale.com>
    Change-Id: I1fc6b71fce4807fdc18a96db0182c15dd9e3c649
    Reviewed-on: http://git.am.freescale.net:8181/36196
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Ramneek Mehresh <ramneek.mehresh@freescale.com>
    Reviewed-by: Honghua Yin <Hong-Hua.Yin@freescale.com>

--- drivers/usb/gadget/udc/fsl_udc_core.c
+++ drivers/usb/gadget/udc/fsl_udc_core.c
@@ -1691,7 +1691,7 @@ static void dtd_complete_irq(struct fsl_udc *udc)
 		curr_ep = get_ep_by_pipe(udc, i);
 
 		/* If the ep is configured */
-		if (curr_ep->name == NULL) {
+		if (strncmp(curr_ep->name, "ep", 2)) {
 			WARNING("Invalid EP?");
 			continue;
 		}
