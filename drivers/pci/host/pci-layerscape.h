0c34925d214b6706b7c45008bfc94252a2d7a4cb
Varun Sethi <Varun.Sethi@freescale.com>
2015-08-23 19:23:32 +0530

    Fix copyright for pci-layerscape.h.
    Signed-off-by: Varun Sethi <Varun.Sethi@freescale.com>

--- drivers/pci/host/pci-layerscape.h
+++ drivers/pci/host/pci-layerscape.h
@@ -1,4 +1,8 @@
 /*
+ * Copyright (C) 2015 Freescale Semiconductor.
+ *
+ * Author: Varun Sethi <Varun.Sethi@freescale.com>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
