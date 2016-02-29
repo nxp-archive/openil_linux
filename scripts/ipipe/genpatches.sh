6259eabf1d0d9fcc8ccb011ea8b165feaa05a954
Philippe Gerum <rpm@xenomai.org>
2016-02-29 10:06:42 +0100

    ipipe: fixup genpatches script

--- scripts/ipipe/genpatches.sh
+++ scripts/ipipe/genpatches.sh
@@ -59,6 +59,7 @@ git diff "$reference" | awk -v kvers="$kvers" -v splitmode="$split" \
 BEGIN {
     driver_arch["cpuidle/Kconfig"]="noarch"
     driver_arch["tty/serial/8250/8250_core.c"]="noarch"
+    driver_arch["iommu/irq_remapping.c"]="noarch"
 
     driver_arch["clk/mxs/clk-imx28.c"]="arm"
     driver_arch["clocksource/arm_arch_timer.c"]="arm"
