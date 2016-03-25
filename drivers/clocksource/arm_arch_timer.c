nxp-archive/openil_linux

commit 344ca1046f768b0b59609cfbd83922ea5fdf1ea8

clocksource: arm_arch_timer: Timer reread erratum
Work around Freescale erratum A-008585 ("ARM Generic Timer may contain
an erroneous value"), which says that the TVAL and CNTnCT registers may
"contain an erroneous value for a small number of core clock cycles
every time the timer value changes".  The workaround is to read the
register twice, with back-to-back instructions, and repeat if the
values do not match.

This erratum is present on LS2080A (64-bit), LS1043A (64-bit), LS1021A
(32-bit) and derivatives.  This patch only addresses 64-bit.

This patch is loosely based on work by Priyanka Jain and Bhupesh
Sharma.

Signed-off-by: Scott Wood <scott.wood@nxp.com>
(Stuart: split out device tree change)
Signed-off-by: Stuart Yoder <stuart.yoder@nxp.com>

drivers/clocksource/arm_arch_timer.c
@@ -71,6 +71,10 @@ static bool arch_timer_use_virtual = true;
 static bool arch_timer_c3stop;
 static bool arch_timer_mem_use_virtual;
 
+#ifndef arm_arch_timer_reread
+bool arm_arch_timer_reread;
+#endif
+
 /*
  * Architected system timer support.
  */
@@ -736,6 +740,10 @@ static void __init arch_timer_of_init(struct device_node *np)
 	arch_timer_detect_rate(NULL, np);
 
 	arch_timer_c3stop = !of_property_read_bool(np, "always-on");
+
+#ifndef arm_arch_timer_reread
+	arm_arch_timer_reread = of_property_read_bool(np, "arm,reread-timer");
+#endif
 
 	/*
 	 * If we cannot rely on firmware initializing the timer registers then

arch/arm64/include/asm/arch_timer.h
@@ -27,6 +27,34 @@
 
 #include <clocksource/arm_arch_timer.h>
 
+#ifdef CONFIG_ARCH_LAYERSCAPE
+extern bool arm_arch_timer_reread;
+#else
+#define arm_arch_timer_reread false
+#endif
+
+#define ARCH_TIMER_REREAD(reg) ({ \
+	u64 _val_old, _val_new; \
+	int _timeout = 200; \
+	do { \
+		asm volatile("mrs %0, " reg ";" \
+			     "mrs %1, " reg \
+			     : "=r" (_val_old), "=r" (_val_new)); \
+		_timeout--; \
+	} while (_val_old != _val_new && _timeout); \
+	WARN_ON_ONCE(_timeout <= 0 && _val_old != _val_new); \
+	_val_old; \
+})
+
+#define ARCH_TIMER_READ(reg) ({ \
+	u64 _val; \
+	if (arm_arch_timer_reread) \
+		_val = ARCH_TIMER_REREAD(reg); \
+	else \
+		asm volatile("mrs %0, " reg : "=r" (_val)); \
+	_val; \
+})
+
 /*
  * These register accessors are marked inline so the compiler can
  * nicely work out which register we want, and chuck away the rest of
@@ -69,7 +97,7 @@ u32 arch_timer_reg_read_cp15(int access, enum arch_timer_reg reg)
 			asm volatile("mrs %0,  cntp_ctl_el0" : "=r" (val));
 			break;
 		case ARCH_TIMER_REG_TVAL:
-			asm volatile("mrs %0, cntp_tval_el0" : "=r" (val));
+			val = ARCH_TIMER_READ("cntp_tval_el0");
 			break;
 		}
 	} else if (access == ARCH_TIMER_VIRT_ACCESS) {
@@ -78,7 +106,7 @@ u32 arch_timer_reg_read_cp15(int access, enum arch_timer_reg reg)
 			asm volatile("mrs %0,  cntv_ctl_el0" : "=r" (val));
 			break;
 		case ARCH_TIMER_REG_TVAL:
-			asm volatile("mrs %0, cntv_tval_el0" : "=r" (val));
+			val = ARCH_TIMER_READ("cntv_tval_el0");
 			break;
 		}
 	}
@@ -116,12 +144,8 @@ static inline u64 arch_counter_get_cntpct(void)
 
 static inline u64 arch_counter_get_cntvct(void)
 {
-	u64 cval;
-
 	isb();
-	asm volatile("mrs %0, cntvct_el0" : "=r" (cval));
-
-	return cval;
+	return ARCH_TIMER_READ("cntvct_el0");
 }
 
 static inline int arch_timer_arch_init(void)

arch/arm64/include/asm/vdso_datapage.h
@@ -34,6 +34,7 @@ struct vdso_data {
 	__u32 tz_minuteswest;	/* Whacky timezone stuff */
 	__u32 tz_dsttime;
 	__u32 use_syscall;
+	__u32 timer_reread;	/* Erratum requires two equal timer reads */
 };
 
 #endif /* !__ASSEMBLY__ */

arch/arm64/kernel/asm-offsets.c
@@ -94,6 +94,7 @@ int main(void)
   DEFINE(VDSO_TZ_MINWEST,	offsetof(struct vdso_data, tz_minuteswest));
   DEFINE(VDSO_TZ_DSTTIME,	offsetof(struct vdso_data, tz_dsttime));
   DEFINE(VDSO_USE_SYSCALL,	offsetof(struct vdso_data, use_syscall));
+  DEFINE(VDSO_TIMER_REREAD,	offsetof(struct vdso_data, timer_reread));
   BLANK();
   DEFINE(TVAL_TV_SEC,		offsetof(struct timeval, tv_sec));
   DEFINE(TVAL_TV_USEC,		offsetof(struct timeval, tv_usec));

arch/arm64/kernel/vdso.c
@@ -31,6 +31,7 @@
 #include <linux/timekeeper_internal.h>
 #include <linux/vmalloc.h>
 
+#include <asm/arch_timer.h>
 #include <asm/cacheflush.h>
 #include <asm/signal32.h>
 #include <asm/vdso.h>
@@ -206,6 +207,7 @@ void update_vsyscall(struct timekeeper *tk)
 	smp_wmb();
 
 	xtime_coarse = __current_kernel_time();
+	vdso_data->timer_reread			= arm_arch_timer_reread;
 	vdso_data->use_syscall			= use_syscall;
 	vdso_data->xtime_coarse_sec		= xtime_coarse.tv_sec;
 	vdso_data->xtime_coarse_nsec		= xtime_coarse.tv_nsec;

arch/arm64/kernel/vdso/gettimeofday.S
@@ -207,7 +207,7 @@ ENDPROC(__kernel_clock_getres)
 /*
  * Read the current time from the architected counter.
  * Expects vdso_data to be initialised.
- * Clobbers the temporary registers (x9 - x15).
+ * Clobbers the temporary registers (x9 - x17).
  * Returns:
  *  - w9		= vDSO sequence counter
  *  - (x10, x11)	= (ts->tv_sec, shifted ts->tv_nsec)
@@ -217,6 +217,9 @@ ENTRY(__do_get_tspec)
 	.cfi_startproc
 
 	/* Read from the vDSO data page. */
+#ifdef CONFIG_ARCH_LAYERSCAPE
+	ldr	w17, [vdso_data, #VDSO_TIMER_REREAD]
+#endif
 	ldr	x10, [vdso_data, #VDSO_CS_CYCLE_LAST]
 	ldp	x13, x14, [vdso_data, #VDSO_XTIME_CLK_SEC]
 	ldp	w11, w12, [vdso_data, #VDSO_CS_MULT]
@@ -225,6 +228,19 @@ ENTRY(__do_get_tspec)
 	/* Read the virtual counter. */
 	isb
 	mrs	x15, cntvct_el0
+#ifdef CONFIG_ARCH_LAYERSCAPE
+	/*
+	 * Erratum A-008585 requires back-to-back reads to be identical
+	 * in order to avoid glitches.
+	 */
+	cmp	w17, #0
+	b.eq	2f
+1:	mrs	x15, cntvct_el0
+	mrs	x16, cntvct_el0
+	cmp	x16, x15
+	b.ne	1b
+2:
+#endif
 
 	/* Calculate cycle delta and convert to ns. */
 	sub	x10, x15, x10

commit fed8a9b7b6d35383da43a5563c6a631c1141d84f

clocksource: arch_timer: export arm_arch_timer_reread symbol
The symbol needs to be accesible by modules, for example tcrypt,
thus export it:

ERROR: "arm_arch_timer_reread" [crypto/tcrypt.ko] undefined!
make[1]: *** [__modpost] Error 1
make: *** [modules] Error 2
make: *** Waiting for unfinished jobs....

Fixes: 344ca1046f768 ("clocksource: arm_arch_timer: Timer reread erratum")
Signed-off-by: Horia Geantă <horia.geanta@nxp.com>

drivers/clocksource/arm_arch_timer.c
@@ -75,6 +75,7 @@ static __always_inline u32 arch_timer_reg_read(int access,
 
 #ifndef arm_arch_timer_reread
 bool arm_arch_timer_reread;
+EXPORT_SYMBOL(arm_arch_timer_reread);
 #endif
 
 /*