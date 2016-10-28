a31813708f899a103b23492204d107c3c0feaa24
Madalin Bucur <madalin.bucur@nxp.com>
2016-10-28 10:48:00 +0300

    dpaa_eth: rework runtime support for erratum A010022
    Address both ARM 64 bit and 32 bit configurations.
    Simplify the codebase, add explicit erratum name.
    
    Signed-off-by: Madalin Bucur <madalin.bucur@nxp.com>

--- drivers/net/ethernet/freescale/sdk_dpaa/dpaa_eth_common.h
+++ drivers/net/ethernet/freescale/sdk_dpaa/dpaa_eth_common.h
@@ -58,11 +58,10 @@
 #define DPA_SGT_MAX_ENTRIES 16 /* maximum number of entries in SG Table */
 
 #define DPA_SGT_ENTRIES_THRESHOLD	DPA_SGT_MAX_ENTRIES
-#ifdef CONFIG_ARM64
-/* each S/G entry can be divided into two S/G entries */
+#ifndef CONFIG_PPC
+/* each S/G entry can be divided into two S/G entries during errata W/A */
 #define DPA_SGT_4K_ENTRIES_THRESHOLD	7
-#endif /* CONFIG_ARM64 */
-
+#endif
 
 #define DPA_BUFF_RELEASE_MAX 8 /* maximum number of buffers released at once */
 
