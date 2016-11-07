8ca3d9d8847d819bb844b472bb262d0bbdad529a
Bharat Bhushan <Bharat.Bhushan@nxp.com>
2016-11-07 16:57:45 +0530

    ARM64/PCI: Allow resources to be mmaped
    On ARM64, resources were not allowed to be mmaped. This patch
    enables same.
    
    Signed-off-by: Bharat Bhushan <Bharat.Bhushan@nxp.com>

--- arch/arm64/include/asm/pci.h
+++ arch/arm64/include/asm/pci.h
@@ -33,6 +33,9 @@ static inline int pci_get_legacy_ide_irq(struct pci_dev *dev, int channel)
 	return -ENODEV;
 }
 
+#define HAVE_PCI_MMAP
+extern int pci_mmap_page_range(struct pci_dev *dev, struct vm_area_struct *vma,
+			       enum pci_mmap_state mmap_state, int write_combine);
 static inline int pci_proc_domain(struct pci_bus *bus)
 {
 	return 1;
