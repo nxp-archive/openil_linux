69b514ca4a09eb7c7885c79ba754733d97c19629
Roy Pledge <roy.pledge@nxp.com>
2016-11-29 17:22:38 -0500

    Revert "fsl_qbman: Use cache inhibited portals on LS1046"
    This reverts commit 98feb2305e4b39c459e30dbe8867e3fcccf1f6c1.

--- drivers/staging/fsl_qbman/qman_high.c
+++ drivers/staging/fsl_qbman/qman_high.c
@@ -579,7 +579,7 @@ struct qman_portal *qman_create_portal(
         portal->use_eqcr_ci_stashing = ((qman_ip_rev >= QMAN_REV30) ?
 					1 : 0);
 #elif defined(CONFIG_ARM) || defined(CONFIG_ARM64)
-	portal->use_eqcr_ci_stashing = !config->cache_inhibited;
+	portal->use_eqcr_ci_stashing = 1;
 #else
         portal->use_eqcr_ci_stashing = 0;
 #endif
