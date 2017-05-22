0c75a8f31774751e827af468f92adc31c44aba1b
Changming Huang <jerry.huang@nxp.com>
2017-05-22 14:06:44 +0800

    Revert "PCI: handle PCI IRQs within xenomai domain"
    This reverts commit b8993d7ebf0f89455d6a283de36a7de876a8a401.

--- drivers/pci/host/pci-layerscape.c
+++ drivers/pci/host/pci-layerscape.c
@@ -25,7 +25,6 @@
 #include <linux/regmap.h>
 #include <linux/list.h>
 #include <linux/syscore_ops.h>
-#include <rtdm/driver.h>
 
 #include "pcie-designware.h"
 
@@ -445,9 +444,9 @@ static void ls_pcie_host_hack_pm_init(struct ls_pcie *pcie)
 	of_node_put(np);
 }
 
-static int ls_pcie_pme_irq_handler(rtdm_irq_t *irq_context)
+static irqreturn_t ls_pcie_pme_irq_handler(int irq, void *data)
 {
-	struct pcie_port *pp = rtdm_irq_get_arg(irq_context, struct pcie_port);
+	struct pcie_port *pp = data;
 	struct ls_pcie *pcie = to_ls_pcie(pp);
 	u32 val;
 
@@ -462,7 +461,6 @@ static int ls_pcie_pme_irq_handler(rtdm_irq_t *irq_context)
 	return IRQ_HANDLED;
 }
 
-static rtdm_irq_t irq_handle;
 static int ls_pcie_host_pme_init(struct ls_pcie *pcie,
 				 struct platform_device *pdev)
 {
@@ -484,9 +482,8 @@ static int ls_pcie_host_pme_init(struct ls_pcie *pcie,
 		return pcie->pme_irq;
 	}
 
-	ret = rtdm_irq_request(&irq_handle, pcie->pme_irq,
-				ls_pcie_pme_irq_handler, RTDM_IRQTYPE_SHARED,
-				"ls-pcie-pme", pp);
+	ret = devm_request_irq(pp->dev, pcie->pme_irq, ls_pcie_pme_irq_handler,
+			       IRQF_SHARED, "ls-pcie-pme", pp);
 	if (ret) {
 		dev_err(pp->dev, "Failed to request pme irq\n");
 		return ret;
