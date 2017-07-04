2fa60da13cb6ba7fc1471be9ff9989644c3c26d5
xu chen <xu.chen_3@nxp.com>
2017-07-04 09:03:27 +0800

    Xenomai3 Cobalt kernel on arm64

--- arch/arm64/xenomai/include/asm/xenomai/uapi/features.h
+++ arch/arm64/xenomai/include/asm/xenomai/uapi/features.h
@@ -22,7 +22,7 @@
 #define _COBALT_ARM64_ASM_UAPI_FEATURES_H
 
 /* The ABI revision level we use on this arch. */
-#define XENOMAI_ABI_REV   17UL
+#define XENOMAI_ABI_REV   1UL
 
 #define XENOMAI_FEAT_DEP (__xn_feat_generic_mask)
 
