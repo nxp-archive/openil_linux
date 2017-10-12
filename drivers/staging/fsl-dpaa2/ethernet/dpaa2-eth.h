c89bf59d8ca022c8cfa1438b4ecc318f06654d38
Bogdan Purcareata <bogdan.purcareata@nxp.com>
2017-10-12 12:55:41 +0000

    staging: fsl-dpaa2/eth: Use signed variables for buffer accounting
    When comparing each channel buf_count with the refill threshold and max
    buffer per channel thershold, there's an implicit conversion to unsigned
    which leads to underflow errors when per channel buffers are consumed
    too fast.
    
    Convert all variables to use signed types.
    
    Signed-off-by: Bogdan Purcareata <bogdan.purcareata@nxp.com>

--- drivers/staging/fsl-dpaa2/ethernet/dpaa2-eth.h
+++ drivers/staging/fsl-dpaa2/ethernet/dpaa2-eth.h
@@ -390,8 +390,8 @@ struct dpaa2_eth_priv {
 	u16 tx_qdid;
 	u16 rx_buf_align;
 	struct iommu_domain *iommu_domain;
-	u32 max_bufs_per_ch;
-	u32 refill_thresh;
+	int max_bufs_per_ch;
+	int refill_thresh;
 	bool has_xdp_prog;
 
 	void *cscn_mem;	/* Tx congestion notifications are written here */
