66b4af79b6d6634b4e82befb535d10a08f9c0120
Philippe Gerum <rpm@xenomai.org>
2017-12-03 12:06:54 +0100

    ftrace: ipipe: enable tracing from the head domain
    Enabling ftrace for a co-kernel running in the head domain of a
    pipelined interrupt context means to:
    
    - make sure that ftrace's live kernel code patching still runs
      unpreempted by any head domain activity (so that the latter can't
      tread on invalid or half-baked changes in the .text section).
    
    - allow the co-kernel code running in the head domain to traverse
      ftrace's tracepoints safely.
    
    The changes introduced by this commit ensure this by fixing up some
    key critical sections so that interrupts are still disabled in the
    CPU, undoing the interrupt flag virtualization in those particular
    cases.

--- kernel/trace/trace_preemptirq.c
+++ kernel/trace/trace_preemptirq.c
@@ -20,7 +20,7 @@ static DEFINE_PER_CPU(int, tracing_irq_cpu);
 
 void trace_hardirqs_on(void)
 {
-	if (this_cpu_read(tracing_irq_cpu)) {
+	if (ipipe_root_p && this_cpu_read(tracing_irq_cpu)) {
 		if (!in_nmi())
 			trace_irq_enable_rcuidle(CALLER_ADDR0, CALLER_ADDR1);
 		tracer_hardirqs_on(CALLER_ADDR0, CALLER_ADDR1);
@@ -33,7 +33,7 @@ EXPORT_SYMBOL(trace_hardirqs_on);
 
 void trace_hardirqs_off(void)
 {
-	if (!this_cpu_read(tracing_irq_cpu)) {
+	if (ipipe_root_p && !this_cpu_read(tracing_irq_cpu)) {
 		this_cpu_write(tracing_irq_cpu, 1);
 		tracer_hardirqs_off(CALLER_ADDR0, CALLER_ADDR1);
 		if (!in_nmi())
@@ -46,7 +46,7 @@ EXPORT_SYMBOL(trace_hardirqs_off);
 
 __visible void trace_hardirqs_on_caller(unsigned long caller_addr)
 {
-	if (this_cpu_read(tracing_irq_cpu)) {
+	if (ipipe_root_p && this_cpu_read(tracing_irq_cpu)) {
 		if (!in_nmi())
 			trace_irq_enable_rcuidle(CALLER_ADDR0, caller_addr);
 		tracer_hardirqs_on(CALLER_ADDR0, caller_addr);
@@ -59,7 +59,7 @@ EXPORT_SYMBOL(trace_hardirqs_on_caller);
 
 __visible void trace_hardirqs_off_caller(unsigned long caller_addr)
 {
-	if (!this_cpu_read(tracing_irq_cpu)) {
+	if (ipipe_root_p && !this_cpu_read(tracing_irq_cpu)) {
 		this_cpu_write(tracing_irq_cpu, 1);
 		tracer_hardirqs_off(CALLER_ADDR0, caller_addr);
 		if (!in_nmi())
@@ -75,14 +75,14 @@ EXPORT_SYMBOL(trace_hardirqs_off_caller);
 
 void trace_preempt_on(unsigned long a0, unsigned long a1)
 {
-	if (!in_nmi())
+	if (ipipe_root_p && !in_nmi())
 		trace_preempt_enable_rcuidle(a0, a1);
 	tracer_preempt_on(a0, a1);
 }
 
 void trace_preempt_off(unsigned long a0, unsigned long a1)
 {
-	if (!in_nmi())
+	if (ipipe_root_p && !in_nmi())
 		trace_preempt_disable_rcuidle(a0, a1);
 	tracer_preempt_off(a0, a1);
 }
