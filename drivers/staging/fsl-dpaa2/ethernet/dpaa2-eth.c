nxp-qoriq/linux

staging: fsl-dpaa2/eth: Configure HWA on XDP_TX FD
Explicitly reserve a hardware annotation area of 128B in the XDP_TX FD,
since we write to the HWA[FAEAD] field to disable Tx confirmations.

We don't actually use the software annotation area, but since we reuse
the Rx FD and buffer, let the hardware know the SWA is reserved.

Signed-off-by: Ioana Radulescu <ruxandra.radulescu@nxp.com>

drivers/staging/fsl-dpaa2/ethernet/dpaa2-eth.c
@@ -245,6 +245,7 @@ static int dpaa2_eth_xdp_tx(struct dpaa2_eth_priv *priv,
 	/* Mark the egress frame annotation area as valid */
 	frc = dpaa2_fd_get_frc(fd);
 	dpaa2_fd_set_frc(fd, frc | DPAA2_FD_FRC_FAEADV);
+	dpaa2_fd_set_ctrl(fd, FD_CTRL_PTA | DPAA2_FD_CTRL_ASAL);
 
 	ctrl = DPAA2_FAEAD_A4V | DPAA2_FAEAD_A2V | DPAA2_FAEAD_EBDDV;
 	faead = dpaa2_eth_get_faead(buf_start);

nxp-archive/openil_linux

drivers/staging/fsl-dpaa2/ethernet/dpaa2-eth.c
@@ -236,6 +236,7 @@ static int dpaa2_eth_xdp_tx(struct dpaa2_eth_priv *priv,
 	/* Mark the egress frame annotation area as valid */
 	frc = dpaa2_fd_get_frc(fd);
 	dpaa2_fd_set_frc(fd, frc | DPAA2_FD_FRC_FAEADV);
+	dpaa2_fd_set_ctrl(fd, DPAA2_FD_CTRL_PTA | DPAA2_FD_CTRL_ASAL);
 
 	ctrl = DPAA2_FAEAD_A4V | DPAA2_FAEAD_A2V | DPAA2_FAEAD_EBDDV;
 	faead = dpaa2_get_faead(buf_start);