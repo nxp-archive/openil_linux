2907703bb65e86180bacc86cb3770c282092350a
Claudiu Manoil <claudiu.manoil@nxp.com>
2018-04-25 15:43:34 +0300

    enetc: ethtool: Remove register value debug prints
    Signed-off-by: Claudiu Manoil <claudiu.manoil@nxp.com>

--- drivers/net/ethernet/freescale/enetc/enetc_ethtool.c
+++ drivers/net/ethernet/freescale/enetc/enetc_ethtool.c
@@ -34,22 +34,6 @@
 
 #include "enetc.h"
 
-#define PR_REG_FMT_STR(NAME) __stringify(NAME) "[0x%X]: %08x\n"
-#define PR_BDR_REG_FMT_STR(NAME) "ring %d: " PR_REG_FMT_STR(NAME)
-
-#define PR_REG(hw, NAME)	pr_info(PR_REG_FMT_STR(NAME), ENETC_##NAME, \
-					enetc_rd(hw, ENETC_##NAME))
-#define PR_PREG(hw, NAME)	pr_info(PR_REG_FMT_STR(NAME), ENETC_##NAME, \
-					enetc_port_rd(hw, ENETC_##NAME))
-#define PR_GREG(hw, NAME)	pr_info(PR_REG_FMT_STR(NAME), ENETC_##NAME, \
-					enetc_global_rd(hw, ENETC_##NAME))
-#define enetc_rxbdr_off(i, off) ENETC_BDR(RX, i, (off))
-#define enetc_txbdr_off(i, off) ENETC_BDR(TX, i, (off))
-#define PR_BDR_REG(hw, t, i, NAME) \
-			pr_info(PR_BDR_REG_FMT_STR(NAME), i, \
-				enetc_##t##bdr_off(i, ENETC_##NAME), \
-				enetc_##t##bdr_rd(hw, i, ENETC_##NAME))
-
 static const u32 enetc_si_regs[] = {
 	ENETC_SIMR, ENETC_SIPMAR0, ENETC_SIPMAR1, ENETC_SICBDRMR,
 	ENETC_SICBDRSR,	ENETC_SICBDRBAR0, ENETC_SICBDRBAR1, ENETC_SICBDRCIR,
@@ -91,8 +75,6 @@ static int enetc_get_reglen(struct net_device *ndev)
 	return len;
 }
 
-static void enetc_get_regs_debug(struct net_device *ndev);
-
 static void enetc_get_regs(struct net_device *ndev, struct ethtool_regs *regs,
 			   void *regbuf)
 {
@@ -132,84 +114,6 @@ static void enetc_get_regs(struct net_device *ndev, struct ethtool_regs *regs,
 		addr = ENETC_PORT_BASE + enetc_port_regs[i];
 		*buf++ = addr;
 		*buf++ = enetc_rd(hw, addr);
-	}
-
-	enetc_get_regs_debug(ndev);
-}
-
-static void enetc_get_regs_debug(struct net_device *ndev)
-{
-	struct enetc_ndev_priv *priv = netdev_priv(ndev);
-	struct enetc_hw *hw = &priv->si->hw;
-	int i;
-
-	/* SI regs */
-	PR_REG(hw, SIMR);
-	PR_REG(hw, SIPMAR0);
-
-	/** Control ring regs */
-	PR_REG(hw, SICBDRMR);
-	PR_REG(hw, SICBDRSR);
-	PR_REG(hw, SICBDRBAR0);
-	PR_REG(hw, SICBDRBAR1);
-	PR_REG(hw, SICBDRCIR);
-	PR_REG(hw, SICBDRCISR);
-	PR_REG(hw, SICBDRLENR);
-
-	PR_REG(hw, SIPMAR1);
-	PR_REG(hw, SICAPR0);
-	PR_REG(hw, SICAPR1);
-
-	PR_REG(hw, SIMSIVR);
-	for (i = 0; i < priv->bdr_int_num; i++) {
-		PR_REG(hw, SIMSITRV(i));
-		PR_REG(hw, SIMSIRRV(i));
-	}
-
-	PR_REG(hw, PRFSCAPR);
-	/** Tx BDR dump */
-	for (i = 0; i < priv->num_tx_rings; i++) {
-		PR_BDR_REG(hw, tx, i, TBMR);
-		PR_BDR_REG(hw, tx, i, TBSR);
-		PR_BDR_REG(hw, tx, i, TBBAR0);
-		PR_BDR_REG(hw, tx, i, TBBAR1);
-		PR_BDR_REG(hw, tx, i, TBCIR);
-		PR_BDR_REG(hw, tx, i, TBCISR);
-		PR_BDR_REG(hw, tx, i, TBLENR);
-		PR_BDR_REG(hw, tx, i, TBIER);
-	}
-	/** Rx BDR dump */
-	for (i = 0; i < priv->num_rx_rings; i++) {
-		PR_BDR_REG(hw, rx, i, RBMR);
-		PR_BDR_REG(hw, rx, i, RBSR);
-		PR_BDR_REG(hw, rx, i, RBBSR);
-		PR_BDR_REG(hw, rx, i, RBCIR);
-		PR_BDR_REG(hw, rx, i, RBBAR0);
-		PR_BDR_REG(hw, rx, i, RBBAR1);
-		PR_BDR_REG(hw, rx, i, RBPIR);
-		PR_BDR_REG(hw, rx, i, RBLENR);
-		PR_BDR_REG(hw, rx, i, RBICIR0);
-		PR_BDR_REG(hw, rx, i, RBIER);
-	}
-
-	if (hw->port) {
-		/* Port regs */
-		PR_PREG(hw, PMR);
-		PR_PREG(hw, PSR);
-		PR_PREG(hw, PSIPMR);
-		PR_PREG(hw, PSIPMAR0(0));
-		PR_PREG(hw, PSIPMAR1(0));
-		PR_PREG(hw, PCAPR0);
-		PR_PREG(hw, PCAPR1);
-		PR_PREG(hw, PSICFGR0(0));
-		PR_PREG(hw, PM0_CMD_CFG);
-		PR_PREG(hw, PM0_MAXFRM);
-	}
-
-	if (hw->global) {
-		/* GLobal regs */
-		PR_GREG(hw, G_EIPBRR0);
-		PR_GREG(hw, G_EIPBRR1);
 	}
 }
 
