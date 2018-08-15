82a21fc92c322b79a7871460a655886cfd105eb2
Alison Wang <alison.wang@nxp.com>
2018-08-15 11:19:19 +0800

    drm: hdp: Add Cadence display controller driver support
    This patch adds Cadence display controller driver support.
    
    Signed-off-by: Sandor Yu <Sandor.yu@nxp.com>
    Signed-off-by: Alison Wang <alison.wang@nxp.com>

---
+++ drivers/gpu/drm/imx/hdp/imx-hdmi.c
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2017-2018 NXP
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ *
+ */
+
+#include <linux/clk.h>
+#include "hdmitx_firmware.h"
+#include "imx-hdp.h"
+#include "imx-hdmi.h"
+#include "API_AFE_ss28fdsoi_kiran_hdmitx.h"
+
+static int character_freq_khz;
+
+void hdmi_fw_load(state_struct *state)
+{
+	CDN_API_LoadFirmware(state,
+		(u8 *)hdmitx_iram0_get_ptr(),
+		hdmitx_iram0_get_size(),
+		(u8 *)hdmitx_dram0_get_ptr(),
+		hdmitx_dram0_get_size());
+}
+
+void hdmi_fw_init(state_struct *state, u32 core_rate)
+{
+	u8 echo_msg[] = "echo test";
+	u8 echo_resp[sizeof(echo_msg) + 1];
+	int ret;
+	u8 sts;
+
+	/* configure the clock */
+	CDN_API_SetClock(state, core_rate/1000000);
+
+	/* moved from CDN_API_LoadFirmware */
+	cdn_apb_write(state, APB_CTRL << 2, 0);
+
+	ret = CDN_API_CheckAlive_blocking(state);
+
+	/* turn on IP activity */
+	ret = CDN_API_MainControl_blocking(state, 1, &sts);
+
+	ret = CDN_API_General_Test_Echo_Ext_blocking(state, echo_msg, echo_resp,
+		 sizeof(echo_msg), CDN_BUS_TYPE_APB);
+}
+
+void hdmi_phy_init(state_struct *state, int vic, int format, int color_depth)
+{
+	int ret;
+
+	/* Configure PHY */
+	character_freq_khz = phy_cfg_hdp_ss28fdsoi(state, 4, vic, color_depth, format);
+
+#ifdef arch_imx
+	hdp_phy_reset(1);
+#endif
+
+	hdmi_tx_kiran_power_configuration_seq(state, 4);
+
+	/* Set the lane swapping */
+	ret = CDN_API_General_Write_Register_blocking(state, ADDR_SOURCD_PHY + (LANES_CONFIG << 2),
+		F_SOURCE_PHY_LANE0_SWAP(3) | F_SOURCE_PHY_LANE1_SWAP(0) |
+		F_SOURCE_PHY_LANE2_SWAP(1) | F_SOURCE_PHY_LANE3_SWAP(2) |
+		F_SOURCE_PHY_COMB_BYPASS(0) | F_SOURCE_PHY_20_10(1));
+}
+
+void hdmi_mode_set(state_struct *state, int vic, int format, int color_depth, int temp)
+{
+	int ret;
+	GENERAL_Read_Register_response regresp;
+
+	/* B/W Balance Type: 0 no data, 1 IT601, 2 ITU709 */
+	BT_TYPE bw_type = 0;
+	/* Mode = 0 - DVI, 1 - HDMI1.4, 2 HDMI 2.0 */
+	HDMI_TX_MAIL_HANDLER_PROTOCOL_TYPE ptype = 1;
+
+	if (vic == VIC_MODE_97_60Hz)
+		ptype = 2;
+
+	ret = CDN_API_HDMITX_Init_blocking(state);
+
+	/* Set HDMI TX Mode */
+	ret = CDN_API_HDMITX_Set_Mode_blocking(state, ptype, character_freq_khz);
+
+	ret = CDN_API_Set_AVI(state, vic, format, bw_type);
+
+	ret =  CDN_API_HDMITX_SetVic_blocking(state, vic, color_depth, format);
+
+	/* adjust the vsync/hsync polarity */
+	CDN_API_General_Read_Register_blocking(
+					state, ADDR_SOURCE_VIF + (HSYNC2VSYNC_POL_CTRL << 2), &regresp);
+	if ((regresp.val & 0x6) != 0)
+		__raw_writel(0x4, state->mem.ss_base);
+
+	msleep(50);
+}
+
+int hdmi_get_edid_block(void *data, u8 *buf, u32 block, size_t len)
+{
+	HDMITX_TRANS_DATA edidResp;
+	state_struct *state = data;
+	CDN_API_STATUS ret = 0;
+
+	memset(&edidResp, 0, sizeof(edidResp));
+	switch (block) {
+	case 0:
+		ret = CDN_API_HDMITX_READ_EDID_blocking(state, 0, 0, &edidResp);
+		break;
+	case 1:
+		ret = CDN_API_HDMITX_READ_EDID_blocking(state, 0, 1, &edidResp);
+		break;
+	case 2:
+		ret = CDN_API_HDMITX_READ_EDID_blocking(state, 1, 0, &edidResp);
+		break;
+	case 3:
+		ret = CDN_API_HDMITX_READ_EDID_blocking(state, 1, 1, &edidResp);
+		break;
+	default:
+		printk(KERN_ERR "EDID block %x read not support\n", block);
+	}
+
+	memcpy(buf, edidResp.buff, 128);
+
+	return ret;
+}
+
+void hdmi_get_hpd_state(state_struct *state, u8 *hpd)
+{
+	CDN_API_HDMITX_GetHpdStatus_blocking(state, hpd);
+}
---
+++ drivers/gpu/drm/imx/hdp/imx-hdmi.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2017-2019 NXP
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ *
+ */
+#ifndef _IMX_HDMI_H_
+#define _IMX_HDMI_H_
+void hdmi_fw_load(state_struct *state);
+void hdmi_fw_init(state_struct *state, u32 rate);
+void hdmi_phy_init(state_struct *state, int vic, int format, int color_depth);
+void hdmi_mode_set(state_struct *state, int vic, int format, int color_depth, int max_link);
+int hdmi_get_edid_block(void *data, u8 *buf, u32 block, size_t len);
+void hdmi_get_hpd_state(state_struct *state, u8 *hpd);
+
+#endif
