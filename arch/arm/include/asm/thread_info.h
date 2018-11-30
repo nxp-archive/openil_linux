65d83870bdf0c46e4d929011814b2a1ad9e04a35
Jan Kiszka <jan.kiszka@siemens.com>
2018-11-30 21:10:00 +0100

    arm/ipipe: Add support for userspace return notifier
    This adds the arm-specific bits to implement the userspace return
    notifier.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- arch/arm/include/asm/thread_info.h
+++ arch/arm/include/asm/thread_info.h
@@ -179,10 +179,12 @@ extern int vfp_restore_user_hwstate(struct user_vfp *,
 #define TIP_MAYDAY	0	/* MAYDAY call is pending */
 #define TIP_NOTIFY	1	/* Notify head domain about kernel events */
 #define TIP_HEAD	2	/* Runs in head domain */
+#define TIP_USERINTRET	3	/* Notify on IRQ/trap return to root userspace */
 
 #define _TIP_MAYDAY	(1 << TIP_MAYDAY)
 #define _TIP_NOTIFY	(1 << TIP_NOTIFY)
 #define _TIP_HEAD	(1 << TIP_HEAD)
+#define _TIP_USERINTRET	(1 << TIP_USERINTRET)
 
 #endif /* __KERNEL__ */
 #endif /* __ASM_ARM_THREAD_INFO_H */
