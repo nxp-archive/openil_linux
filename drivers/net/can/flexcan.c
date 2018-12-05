fed2230b0dc9e58cc19e97df571c136c0d4dc6d9
Pankaj Bansal <pankaj.bansal@nxp.com>
2018-12-05 19:42:28 +0530

    Revert "drivers: net: can: flexcan: Modify the TDC value"
    This patch logically makes sense, however with these changes, the can link
    becomes unstable i.e. more and more errors are detected and finally we get
    the bus off event.
    
    Therefore, revert these changes for time being.
    in Parallel need to check why these changes causing can bus errors
    
    This reverts commit beac27819f5e80eade5c8eac5070815cde8c525c.

--- drivers/net/can/flexcan.c
+++ drivers/net/can/flexcan.c
@@ -1029,7 +1029,8 @@ static void flexcan_set_bittiming(struct net_device *dev)
 				       FLEXCAN_FDCTRL_TDCFAIL);
 
 			/* offset should be within the can FD bit timings */
-			tdcoff = data_bt->phase_seg1 + data_bt->prop_seg;
+			tdcoff = (data_bt->phase_seg1 + data_bt->prop_seg) *
+				  data_bt->brp;
 
 			reg_fdctrl |= FLEXCAN_FDCTRL_TDCOFF(tdcoff);
 			reg_fdctrl |= FLEXCAN_FDCTRL_MBDSR0(3) |
