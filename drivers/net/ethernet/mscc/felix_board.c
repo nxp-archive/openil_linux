b4d2a6f650990b8c967b87e868afda75ccd8590e
Catalin Horghidan <catalin.horghidan@nxp.com>
2018-12-17 11:46:34 +0200

    felix: don't register xmit handler for CPU port
    Signed-off-by: Catalin Horghidan <catalin.horghidan@nxp.com>

--- drivers/net/ethernet/mscc/felix_board.c
+++ drivers/net/ethernet/mscc/felix_board.c
@@ -437,7 +437,7 @@ static int felix_ports_init(struct ocelot *ocelot)
 			goto release_ports;
 		}
 		/* register xmit handler for external ports */
-		if (ndev && port <= FELIX_EXT_CPU_PORT_ID)
+		if (ndev && port != FELIX_EXT_CPU_PORT_ID)
 			felix_register_xmit_handler(ocelot->ports[port], ndev);
 		/* TODO: check if FL phy require phy_start */
 	}
