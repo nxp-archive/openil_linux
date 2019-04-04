c0adc8567fb4cd384ff363e4d9a7614029bb2fe9
Philippe Gerum <rpm@xenomai.org>
2019-04-04 17:06:35 +0200

    ARM: ipipe: bump relase tag

--- arch/arm/include/asm/ipipe.h
+++ arch/arm/include/asm/ipipe.h
@@ -44,7 +44,7 @@ extern unsigned long arm_return_addr(int level);
 #include <linux/jump_label.h>
 #include <linux/ipipe_trace.h>
 
-#define IPIPE_CORE_RELEASE	1
+#define IPIPE_CORE_RELEASE	2
 
 struct ipipe_domain;
 struct timekeeper;
