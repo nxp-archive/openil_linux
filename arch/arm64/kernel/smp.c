911d87fb9f6e8d247e8d8f38ec266c2ad0a9bc81
Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
2019-11-01 16:42:15 +0800

    Baremetal: IPI: IPI support for ARM64
    Support IPI on ARM64.
    
    Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
    Signed-off-by: Jianchao Wang <jianchao.wang@nxp.com>

--- arch/arm64/kernel/smp.c
+++ arch/arm64/kernel/smp.c
@@ -72,8 +72,19 @@ enum ipi_msg_type {
 	IPI_CPU_CRASH_STOP,
 	IPI_TIMER,
 	IPI_IRQ_WORK,
-	IPI_WAKEUP
+	IPI_WAKEUP,
+#ifdef CONFIG_BAREMETAL
+	IPI_BAREMETAL_COMM = 8,
+#endif
 };
+
+#ifdef CONFIG_BAREMETAL
+inline int ipi_baremetal(void)
+{
+	return IPI_BAREMETAL_COMM;
+}
+EXPORT_SYMBOL_GPL(ipi_baremetal);
+#endif
 
 #ifdef CONFIG_HOTPLUG_CPU
 static int op_cpu_kill(unsigned int cpu);
@@ -769,6 +780,9 @@ static const char *ipi_types[NR_IPI] __tracepoint_string = {
 	S(IPI_TIMER, "Timer broadcast interrupts"),
 	S(IPI_IRQ_WORK, "IRQ work interrupts"),
 	S(IPI_WAKEUP, "CPU wake-up interrupts"),
+#ifdef CONFIG_BAREMETAL
+	S(IPI_BAREMETAL_COMM, "Baremetal inter-core interrupts"),
+#endif
 };
 
 static void smp_cross_call(const struct cpumask *target, unsigned int ipinr)
@@ -887,6 +901,10 @@ void handle_IPI(int ipinr, struct pt_regs *regs)
 	case IPI_RESCHEDULE:
 		scheduler_ipi();
 		break;
+#ifdef CONFIG_BAREMETAL
+	case IPI_BAREMETAL_COMM:
+		break;
+#endif
 
 	case IPI_CALL_FUNC:
 		irq_enter();
@@ -932,7 +950,6 @@ void handle_IPI(int ipinr, struct pt_regs *regs)
 			  cpu);
 		break;
 #endif
-
 	default:
 		pr_crit("CPU%u: Unknown IPI message 0x%x\n", cpu, ipinr);
 		break;
